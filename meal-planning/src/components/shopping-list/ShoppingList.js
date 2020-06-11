d7cb4496a9e98217af29dc84cf7486199720a169
Michelle Tsun <michelle.tsun@uconn.edu>
2020-06-10 20:07:20 -0400

    requested changes made

--- meal-planning/src/components/shopping-list/ShoppingList.js
+++ meal-planning/src/components/shopping-list/ShoppingList.js
@@ -1,27 +1,14 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
-import IconButton from '@material-ui/core/IconButton';
 import styles from "./ShoppingList.module.scss";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
-}));
-
 
 
 function ShoppingList() {
-  const classes = useStyles();
-  const [checked, setChecked] = React.useState([0]);
+  const [checked, setChecked] = React.useState([-1]);
   const ingredients = ['2 bananas', '1/2 cup flour', '1 tsp salt', '1 tsp vanilla', '3 eggs'];
 
   const handleToggle = (value) => () => {
@@ -42,7 +29,8 @@ function ShoppingList() {
     <div className={styles.header}>
       <h3>Shopping List</h3>
     </div>
-    <List className={classes.root}>
+
+    <List className={styles.root}>
 
       {[0, 1, 2, 3, 4].map((value) => {
         const labelId = `checkbox-list-label-${value}`;

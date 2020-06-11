22f26ed54a26fee8a774215971bff324baf44edd
Michelle Tsun <michelle.tsun@uconn.edu>
2020-06-11 14:25:52 -0400

    added delete functionality to shopping list

--- meal-planning/src/components/shopping-list/ShoppingList.js
+++ meal-planning/src/components/shopping-list/ShoppingList.js
@@ -4,12 +4,16 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Checkbox from '@material-ui/core/Checkbox';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
 import styles from "./ShoppingList.module.scss";
 
 
 function ShoppingList() {
   const [checked, setChecked] = React.useState([-1]);
   const ingredients = ['2 bananas', '1/2 cup flour', '1 tsp salt', '1 tsp vanilla', '3 eggs'];
+  const [currentList, setList] = React.useState(ingredients);
 
   const handleToggle = (value) => () => {
     const currentIndex = checked.indexOf(value);
@@ -24,6 +28,13 @@ function ShoppingList() {
     setChecked(newChecked);
   };
 
+  const handleDelete = (value) => () => {
+    const currentIndex = currentList.indexOf(value);
+    const newList = [...currentList];
+    newList.splice(currentIndex, 1);
+    setList(newList);
+  };
+
   return (
     <>
     <div className={styles.header}>
@@ -32,21 +43,26 @@ function ShoppingList() {
 
     <List className={styles.root}>
 
-      {[0, 1, 2, 3, 4].map((value) => {
-        const labelId = `checkbox-list-label-${value}`;
+      {currentList.map((value) => {
+        const checkLabelId = `checkbox-list-label-${value}`;
 
         return (
-          <ListItem key={value} role={undefined} dense button onClick={handleToggle(value)}>
+          <ListItem key={currentList.indexOf(value)} value={value} role={undefined} dense button onClick={handleToggle(value)}>
             <ListItemIcon>
               <Checkbox
                 edge="start"
                 checked={checked.indexOf(value) !== -1}
                 tabIndex={-1}
                 disableRipple
-                inputProps={{ 'aria-labelledby': labelId }}
+                inputProps={{ 'aria-labelledby': checkLabelId }}
               />
             </ListItemIcon>
-            <ListItemText id={labelId} primary={ingredients[value]} />
+            <ListItemText id={checkLabelId} primary={value} />
+            <ListItemSecondaryAction key={currentList.indexOf(value)} value={value} onClick = {handleDelete(value)}>
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
           </ListItem>
         );
       })}
@@ -54,5 +70,6 @@ function ShoppingList() {
     </>
   );
 }
+
 
 export default ShoppingList;

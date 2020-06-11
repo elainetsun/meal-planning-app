675863525be49dc668d94967fe9222f6c02ca3c8
Elaine Tsun <elaine.tsun3@gmail.com>
2020-06-10 20:13:48 -0400

    ingredient selector comp

---
+++ meal-planning/src/components/ingredient-selector/IngredientSelector.js
@@ -0,0 +1,9 @@
+const IngredientSelector = () => {
+  const rows = [
+    { name: "carrot", quanitiy: 1 },
+    { name: "potato", quanitiy: 3 },
+  ];
+  return <div></div>;
+};
+
+export default IngredientSelector;
